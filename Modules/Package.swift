f2bcb88ed1c1405fcd32c09ca0310dc66b2dc943
Pedro <pedroullmann@hotmail.com>
2022-11-23 00:07:18 -0300

    Add launch screen target

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -6,8 +6,12 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v14)],
     products: [
+        // MARK: - Core
         .library(name: "Core-DesignSystem", targets: ["Core-DesignSystem"]),
         .library(name: "Core-Resources", targets: ["Core-Resources"]),
+
+        // MARK: - Features
+        .library(name: "Feature-LaunchScreen", targets: ["Feature-LaunchScreen"])
     ],
     dependencies: [],
     targets: [
@@ -19,6 +23,10 @@ let package = Package(
             name: "Core-Resources",
             dependencies: [],
             resources: [.process("Resources")]
+        ),
+        .target(
+            name: "Feature-LaunchScreen",
+            dependencies: []
         )
     ]
 )
